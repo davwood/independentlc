87e1dbb15ef0c697764fe607b048934615dcda17
davewood <davwood_1@yahoo.com.au>
2016-09-26 13:05:31 -0600

    basic chart working without redraw

--- app/components/line-chart.js
+++ app/components/line-chart.js
@@ -5,12 +5,9 @@ import { line } from 'd3-shape';
 import { scaleLinear } from 'd3-scale';
 
 export default Ember.Component.extend({
-  margin: { top: 20, right: 20, bottom: 20, left: 80 },
+  margin: {top: 20, right: 20, bottom: 30, left: 50},
   fullWidth: 800,
   fullHeight: 300,
-  // baselineGenerator: line().
-  //   x((d, i) => x(i)).
-  //   y(d => y(d.baseline)),
 
   didInsertElement() {
      this.plot = select(this.element.querySelector('svg'));
@@ -19,29 +16,33 @@ export default Ember.Component.extend({
   didRender() {
     // NOTE: Do things with the DOM after it has rendered.
     const data = this.get('data');
-    const width = this.get('fullWidth') - this.get('margin')['top'] - this.get('margin')['right'];
+    const width = this.get('fullWidth') - this.get('margin')['left'] - this.get('margin')['right'];
     const height = this.get('fullHeight') - this.get('margin')['top'] - this.get('margin')['bottom'];
 
-    const x = scaleLinear().range([0, width]);
-    const y = scaleLinear().range([0, height]);
+    const x = scaleLinear()
+      .domain([0, data.length - 1])
+      .range([0, width]);
+    const y = scaleLinear()
+      .domain([data[0], 0])
+      .range([0, height]);
 
-    const lineGenerator = line().
-    x((d) => d).
-    y((d,i) => i);
-    // this.get('x').domain([0, data.length - 1]);
-    // this.get('y').domain([data[0].balance, 0]);
-    x.domain([0, data.length - 1]);
-    y.domain([data[0], 0]);
+    const lineGenerator = line()
+      .x((d, i) => x(i))
+      .y((d) => y(d));
 
-    this.plot.append("g")
+    const svg = this.plot.attr("width", width + this.get('margin')['left'] + this.get('margin')['right'])
+      .attr("height", height + this.get('margin')['top'] + this.get('margin')['bottom'])
+      .append("g")
+      .attr("transform", "translate(" + this.get('margin')['left'] + "," + this.get('margin')['top'] + ")");
+
+    svg.append("g")
       .attr("class", "axis axis--x")
       .attr("transform", "translate(0," + height + ")")
-      .call(axisBottom(x));
-      //
-    //
-    this.plot.append("g")
+      .call(axisBottom().scale(x).ticks(Math.min(data.length, 30)));
+
+    svg.append("g")
         .attr("class", "axis axis--y")
-        .call(axisLeft(y))
+        .call(axisLeft().scale(y))
       .append("text")
         .attr("class", "axis-title")
         .attr("transform", "rotate(-90)")
@@ -50,7 +51,7 @@ export default Ember.Component.extend({
         .style("text-anchor", "end")
         .text("Price ($)");
 
-    this.plot.append("path")
+    svg.append("path")
       .datum(data)
       .attr("class", "line")
       .attr("d", lineGenerator);
